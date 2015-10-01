98e9ed1ebb4ccf8adb261eb2dd38da6f97684d65
N07070 <nelson.perdriau@gmail.com>
2015-10-01 16:43:04 +0200

    Going strong a the validation of the config file.

--- index.php
+++ index.php
@@ -13,41 +13,58 @@
             if ($value == 1) {
                 error_log("[ OK ] The plugin : ".$plugin_name." is activated !", 0);
                 $valid_plugin_config = 0;
+
                 // Read it's plugin_name.json settings file
                 $plugin_config_file = fopen("plugins/".$plugin_name."/".$plugin_name.".json", "r") or die("Unable to open configuration file for ".$plugin_name."! Is it there ?");
                     $plugin_configuration = json_decode(fread($plugin_config_file, filesize("plugins/".$plugin_name."/".$plugin_name.".json")), true);
                 fclose($plugin_config_file);
-
-                print_r($plugin_configuration);
 
                 foreach ($plugin_configuration as $plugin_config_name => $plugin_config_value) {
                     // Check for the presence of : name, version, license, author, description, settings.
                     if ($plugin_config_name == "name" || $plugin_config_name == "version" || $plugin_config_name == "license" || $plugin_config_name == "author" || $plugin_config_name == "description" || $plugin_config_name == "settings") {
                         $valid_plugin_config = $valid_plugin_config + 1;
                     }
-                    // check if name is a string and the name of the plugin in the config.json file.
-
-                    // check if the version is a number and > 0.
-
-                    // check if the license is a string
-
-                    // check if the author is a string
-
-                    // check if the description is a string
-
-                    // check if the settings is an array
-
-                    // check if settings[height] is an int
-
-                    // check if settings[width] is an int
-
-                    // check if settings[auto-update] is a boolean
-
                 }
                 if ($valid_plugin_config == 6 ) {
-                    error_log("[ OK ] The plugin ".$plugin_name." has been loaded and is ready for use.", 0);
+
+                    // check if name is a string and the name of the plugin in the config.json file && check if the version is a number and > 0.
+                    if (is_string($plugin_configuration["name"]) && is_numeric($plugin_configuration["version"]) && $plugin_configuration["version"] > 0) {
+
+                            // // check if the license is a string
+                            // if (c) {
+                            //
+                            //     // check if the author is a string
+                            //     if (condition) {
+                            //
+                            //         // check if the description is a string
+                            //         if (condition) {
+                            //             # code...
+                            //         } else {
+                            //             # code...
+                            //         }
+                            //
+                            //     } else {
+                            //         # code...
+                            //     }
+                            //
+                            // } else {
+                            //     # code...
+                            // }
+
+                    } else {
+                        error_log("[ ER ] The plugin : ".$plugin_name." has an invalid config file.", 0);
+                    }
+
+                        // check if the settings is an array
+
+                        // check if settings[height] is an int
+
+                        // check if settings[width] is an int
+
+                        // check if settings[auto-update] is a boolean
+                    error_log("[ OK ] The plugin : ".$plugin_name." has been loaded and is ready for use.", 0);
                 } else {
-                    error_log("[ ER ] The config file of ".$plugin_name." is invalid.");
+                    error_log("[ ER ] The plugin : ".$plugin_name." has an invalid config file.", 0);
                 }
                     // If it's correct, then get it's plugin_name.html file, and it's plugin_name.js include that into a <div>
 
