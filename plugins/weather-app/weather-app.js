dbab2bb511637b42b26f043fc2237b08f8537980
N07070 <nelson.perdriau@gmail.com>
2015-10-18 12:47:26 +0200

    Fixed the weather app, added the material icons.

--- plugins/weather-app/weather-app.js
+++ plugins/weather-app/weather-app.js
@@ -3,34 +3,36 @@ var city = "Berlin";
 
 function weather_app(){
 
-    var open_weather_api = "http://api.openweathermap.org/data/2.5/weather?q=" + city;
+    var open_weather_api = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=bd82977b86bf27fb59a04b61b657fb6f";
     var weather;
+
     $.getJSON(open_weather_api, function(data){
+        console.log(data);
         $('#weather-app-now').text(data.weather[0]["main"]);
         if (data.weather[0]["id"] === 800) {
             $('#weather-app-image').attr("src","../plugins/weather-app/img/sunny1.png");
         }
-        else if ( data.weather[0]["id"] >= 900 && data.weather[0]["id"] =< 906) {
+        else if ( data.weather[0]["id"] >= 900 && data.weather[0]["id"] <= 906) {
             // Extreme !
-            $('#weather-app-image').attr("src","../plugins/weather-app/img/extreme.png");
-        }else if (data.weather[0]["id"] >= 800 && data.weather[0]["id"] =< 804) {
+            $('#weather-app-image').attr("src","../plugins/weather-app/img/weahter-lightning.png");
+        }else if (data.weather[0]["id"] >= 800 && data.weather[0]["id"] <= 804) {
             // Clouds !
-            $('#weather-app-image').attr("src","../plugins/weather-app/img/cloudy.png");
-        }else if (data.weather[0]["id"] >= 701 && data.weather[0]["id"] =< 781) {
+            $('#weather-app-image').attr("src","../plugins/weather-app/img/weather-cloudy.png");
+        }else if (data.weather[0]["id"] >= 701 && data.weather[0]["id"] <= 781) {
             // Atmosphere ?
-            $('#weather-app-image').attr("src","../plugins/weather-app/img/atmosphere.png");
-        }else if (data.weather[0]["id"] >= 600 && data.weather[0]["id"] =< 622) {
+            $('#weather-app-image').attr("src","../plugins/weather-app/img/weather-windy.png");
+        }else if (data.weather[0]["id"] >= 600 && data.weather[0]["id"] <= 622) {
             // Snow !
-            $('#weather-app-image').attr("src","../plugins/weather-app/img/snow.png");
-        }else if (data.weather[0]["id"] >= 500 && data.weather[0]["id"] =< 531) {
+            $('#weather-app-image').attr("src","../plugins/weather-app/img/weather-snowy.png");
+        }else if (data.weather[0]["id"] >= 500 && data.weather[0]["id"] <= 531) {
             // Rain
-            $('#weather-app-image').attr("src","../plugins/weather-app/img/rain.png");
-        }else if (data.weather[0]["id"] >= 300 && data.weather[0]["id"] =< 321) {
+            $('#weather-app-image').attr("src","../plugins/weather-app/img/weathe-rainy.png");
+        }else if (data.weather[0]["id"] >= 300 && data.weather[0]["id"] <= 321) {
             // Drizzle
-            $('#weather-app-image').attr("src","../plugins/weather-app/img/sunny0.png");
-        }else if (data.weather[0]["id"] >= 200 && data.weather[0]["id"] =< 232) {
+            $('#weather-app-image').attr("src","../plugins/weather-app/img/weather-sunny.png");
+        }else if (data.weather[0]["id"] >= 200 && data.weather[0]["id"] <= 232) {
             // THUNDER !
-            $('#weather-app-image').attr("src","../plugins/weather-app/img/thunder.png");
+            $('#weather-app-image').attr("src","../plugins/weather-app/img/weather-lightning.png");
         }
     });
 
