fe0a4b0816220d0634a7c3eae6f9231b7ff81ae5
Thijs Alkemade <me@thijsalkema.de>
2015-12-06 12:05:29 +0100

    Use .toLocaleTimeString() instead of constructing the time

--- plugins/time/time.js
+++ plugins/time/time.js
@@ -3,6 +3,6 @@ function time() {
 
     setInterval(function() {
         var time = new Date;
-        $('#time .time_now').text(time.getHours() + ":"  + time.getMinutes() + ":" + time.getSeconds() );
+        $('#time .time_now').text(time.toLocaleTimeString());
     }, 1000);
 }
